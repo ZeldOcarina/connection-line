1ea9581f777bc13fba7aa296444ffe4529c5d885
ZeldOcarina <mattia.r86@gmail.com>
2019-05-16 19:05:30 +0200

    Complete scrolling effect

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,53 @@
-const counterNazioni = $('.counter__counter-nazioni');
-const counterNazioniText = $('.counter__nazioni-text')
+/*********************
+ * COUNTER JAVASCRIPT
+ *********************/
+(function () {
+    const counterNazioni = $('.counter__counter-nazioni');
+    const counterNazioniText = $('.counter__nazioni-text')
 
-counterTraduttori();
+    counterTraduttori();
 
-function counterTraduttori() {
-    $('.counter__counter-traduttori').counterUp({
-        time: 2000,
+    function counterTraduttori() {
+        $('.counter__counter-traduttori').counterUp({
+            time: 2000,
+        });
+        counterNazioni.counterUp({
+            time: 2000,
+            beginAt: 0,
+        });
+    }
+})();
+
+/*************
+ * SCROLLIFY
+ ************/
+
+$(function () {
+    $.scrollify({
+        section: "#section-hero, #counters, #story, #settori",
+        sectionName: false,
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () { },
+        after: function () { },
+        afterResize: function () { },
+        afterRender: function () { }
     });
-    counterNazioni.counterUp({
-        time: 2000,
-        beginAt: 0,
-    });
-}
+});
+
+
+
+
+
+
 
 
 
