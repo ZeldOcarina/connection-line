064fdb44ab116886e483046cd4b9e76f9beb0dda
ZeldOcarina <mattia.r86@gmail.com>
2019-05-16 23:34:57 +0200

    Complete scrolling sidebar

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 /*********************
  * COUNTER JAVASCRIPT
  *********************/
-(function () {
+
+(function counter() {
     const counterNazioni = $('.counter__counter-nazioni');
     const counterNazioniText = $('.counter__nazioni-text')
 
@@ -21,27 +22,69 @@
 /*************
  * SCROLLIFY
  ************/
+(function scrollify() {
+    const sidebar = $('.progressive-sidebar__container');
+    const firstSection = $('.progressive-sidebar__section-1');
+    const secondSection = $('.progressive-sidebar__section-2');
+    const thirdSection = $('.progressive-sidebar__section-3');
+    const fourthSection = $('.progressive-sidebar__section-4');
 
-$(function () {
-    $.scrollify({
-        section: "#section-hero, #counters, #story, #settori",
-        sectionName: false,
-        interstitialSection: "",
-        easing: "easeOutExpo",
-        scrollSpeed: 1100,
-        offset: 0,
-        scrollbars: true,
-        standardScrollElements: "",
-        setHeights: true,
-        overflowScroll: true,
-        updateHash: true,
-        touchScroll: true,
-        before: function () { },
-        after: function () { },
-        afterResize: function () { },
-        afterRender: function () { }
+    const heroSection = $('#section-hero');
+    const countersSection = $('#counters')
+    const storySection = $('#story')
+    const settoriSection = $('#settori')
+
+    function removeSidebarActive() {
+        sidebar.children().removeClass('progressive-sidebar__section--active')
+    }
+
+    function scrollToSection(section) {
+        $('html, body').animate({
+            scrollTop: section.offset().top
+        }, 1100);
+    }
+
+    firstSection.on('click', () => scrollToSection(heroSection));
+    secondSection.on('click', () => scrollToSection(countersSection));
+    thirdSection.on('click', () => scrollToSection(storySection));
+    fourthSection.on('click', () => scrollToSection(settoriSection));
+
+    $(function () {
+        $.scrollify({
+            section: "#section-hero, #counters, #story, #settori",
+            sectionName: false,
+            interstitialSection: "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset: 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            overflowScroll: true,
+            updateHash: true,
+            touchScroll: true,
+            before: function () { },
+            after: function (section) {
+                if (section === 0) {
+                    removeSidebarActive();
+                    firstSection.addClass('progressive-sidebar__section--active');
+                } else if (section === 1) {
+                    removeSidebarActive();
+                    secondSection.addClass('progressive-sidebar__section--active');
+                } else if (section === 2) {
+                    removeSidebarActive();
+                    thirdSection.addClass('progressive-sidebar__section--active');
+                } else if (section === 3) {
+                    removeSidebarActive();
+                    fourthSection.addClass('progressive-sidebar__section--active');
+                }
+            },
+            afterResize: function () { },
+            afterRender: function () { }
+        });
     });
-});
+})();
+
 
 
 
