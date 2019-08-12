6356766bbca41add69275aa2e457c69cacaa8801
unknown <mattia.r86@gmail.com>
2019-08-12 15:29:12 +0200

    Correction of modal to exit on background click, privacy broken link, footer address and insertion of carousel brands

--- src/js/utils/Popup.js
+++ src/js/utils/Popup.js
@@ -1,33 +1,39 @@
 import $ from 'jquery';
 import { popupSection, popupOpener, popupCloseButton, displayNone } from './UiElements';
 import Cookies from 'js-cookie';
+import lastURLword from './LastURLWord';
 
 /***************
  * POPUP LOGIC
  **************/
 
 const popup = () => {
-	const page = window.location.pathname.match(/\/([^\/]+)\/?$/)[1];
+	const page = lastURLword();
 	// Variable to be used in mouseout function
 	let popupState = false;
-	let exitPopup = Cookies.get('modalOpened');
 
 	popupOpener.on('click', openPopup);
 	popupCloseButton.on('click', closePopup);
 
-	if (page !== 'thankyou' && page !== 'privacy' && !exitPopup) {
+	if (page !== 'thankyou' && page !== 'privacy' && !Cookies.get('modalOpened')) {
 		setTimeout(() => {
-			if (!exitPopup) openPopup();
+			if (!Cookies.get('modalOpened')) openPopup();
 		}, 20000);
 
 		$('body').on('mouseout', (e) => {
-			if (!e.relatedTarget && !e.toElement && !exitPopup) {
+			if (!e.relatedTarget && !e.toElement && !Cookies.get('modalOpened')) {
 				openPopup();
 			}
 		});
 
 		$(document).keyup((e) => {
 			if (e.keyCode === 27 && popupState) closePopup(); // esc
+		});
+
+		$(document).click((e) => {
+			if (!$(e.target).closest('.popup__content').length) {
+				closePopup();
+			}
 		});
 	}
 
