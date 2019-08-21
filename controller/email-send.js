5f9d6820501da29131a926ff3bd51032ef33dd31
unknown <mattia.r86@gmail.com>
2019-08-21 19:32:34 +0100

    Fix email files settings

--- controller/email-send.js
+++ controller/email-send.js
@@ -3,7 +3,7 @@ const transporter = require('../controller/nodemailer-setup');
 module.exports = emailSender = (req, res, next) => {
 	let newsletterMessage;
 	console.log(req.files);
-	const links = req.files.map((file) => `https://connectionline.ch/uploads/${file.filename.replace(/ /g, '%20')}`);
+	const links = req.files.map((file) => `http://connectionline.ch/uploads/${file.filename.replace(/ /g, '%20')}`);
 
 	req.body.newsletter_accepted === 'on'
 		? (newsletterMessage = 'Si iscrive alla nostra mailing list.')
@@ -12,7 +12,7 @@ module.exports = emailSender = (req, res, next) => {
 		const message = {
 			from: req.body.email,
 			to: 'connectionlinesagl@gmail.com',
-			//cc: 'info@connectionlinesagl.com',
+			cc: 'info@connectionlinesagl.com',
 			subject: 'Una nuova richiesta dal sito!',
 			html: `
             <h1>È arrivata una nuova richiesta dal sito!</h1>
