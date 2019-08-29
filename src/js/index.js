26aa8cc2e792964b2af5f17ccb8e9afd96d740b9
unknown <mattia.r86@gmail.com>
2019-08-29 17:57:48 +0100

    fix Google Tag Manager Tags not working

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import './vendor/jquery.waypoints.min';
 import 'owl.carousel';
 import 'jquery-scrollify';
 import './vendor/jquery.counterup.min';
+import googleTagManager from './utils/googleTagManager';
 import navigationHandler from './utils/Navigation';
 import counter from './utils/Counter';
 import scrollify from './utils/Scrollify';
@@ -11,9 +12,10 @@ import popup from './utils/Popup';
 import cookies from './utils/Cookies';
 import smallText from './utils/SmallText';
 import inputFile from './utils/InputFile';
-import googleTagManager from './utils/googleTagManager';
+
 //import aws_s3 from './utils/aws_s3';
 
+googleTagManager(window, document, 'script', 'dataLayer', 'GTM-PRTTW9W');
 navigationHandler();
 counter();
 scrollify();
@@ -22,5 +24,4 @@ popup();
 cookies();
 smallText();
 inputFile();
-googleTagManager(window, document, 'script', 'dataLayer', 'GTM-WWXT6BH');
 //aws_s3();
